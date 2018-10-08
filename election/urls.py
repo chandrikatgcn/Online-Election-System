5ee98ef309998886d18964dabddd9e6f2cc82655
chandrikatgcn <chandrikatgcn@gmail.com>
2018-10-08 09:11:22 +0530

    staticfiles_modifications

--- election/urls.py
+++ election/urls.py
@@ -6,7 +6,7 @@ admin.site.site_header = 'Election administration'
 
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
-    path('', views.SignUpView.as_view(), name='signup'),
+    path('accounts/signup/', views.SignUpView.as_view(), name='signup'),
     path('accounts/signup/candidate/', views.CandidateSignUpView.as_view(), name='candidate_signup'),
     path('accounts/signup/voter/', views.VoterSignUpView.as_view(), name='voter_signup'),
     path('accounts/login/voter_login/', views.login_voter, name='voter_login'),
